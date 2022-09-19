gfk-kube/kubeedge-lite

Merge pull request from GHSA-vwm6-qc77-v2rh
* fix invalid request

Signed-off-by: vincentgoat <linguohui1@huawei.com>

* fix CI issue

Signed-off-by: Kevin Wang <kevinwzf0126@gmail.com>

Co-authored-by: Kevin Wang <kevinwzf0126@gmail.com>
Former-commit-id: f2c6868e1bf85be7c137636656294db348b93f0d

cloud/pkg/router/listener/http.go
@@ -157,7 +157,7 @@ func (rh *RestHandler) httpHandler(w http.ResponseWriter, r *http.Request) {
 			klog.Errorf("response convert error, msg id: %s", msgID)
 			return
 		}
-		body, err := io.ReadAll(response.Body)
+		body, err := io.ReadAll(io.LimitReader(response.Body, MaxMessageBytes))
 		if err != nil {
 			klog.Errorf("response body read error, msg id: %s, reason: %v", msgID, err)
 			return

edge/pkg/edgehub/certificate/certmanager.go
@@ -254,8 +254,7 @@ func GetCACert(url string) ([]byte, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
-
-	caCert, err := io.ReadAll(res.Body)
+	caCert, err := io.ReadAll(io.LimitReader(res.Body, constants.MaxRespBodyLength))
 	if err != nil {
 		return nil, err
 	}
@@ -286,7 +285,7 @@ func (cm *CertManager) GetEdgeCert(url string, capem []byte, cert tls.Certificat
 	}
 	defer res.Body.Close()
 
-	content, err := io.ReadAll(res.Body)
+	content, err := io.ReadAll(io.LimitReader(res.Body, constants.MaxRespBodyLength))
 	if err != nil {
 		return nil, nil, err
 	}

pkg/stream/message.go
@@ -23,6 +23,8 @@ import (
 	"io"
 
 	"k8s.io/klog/v2"
+
+	"github.com/kubeedge/kubeedge/common/constants"
 )
 
 type MessageType uint64
@@ -80,7 +82,7 @@ func ReadMessageFromTunnel(r io.Reader) (*Message, error) {
 	if err != nil {
 		return nil, err
 	}
-	data, err := io.ReadAll(buf)
+	data, err := io.ReadAll(io.LimitReader(buf, constants.MaxRespBodyLength))
 	if err != nil {
 		return nil, err
 	}

staging/src/github.com/kubeedge/viaduct/pkg/client/ws.go
@@ -8,6 +8,7 @@ import (
 	"k8s.io/klog/v2"
 
 	"github.com/kubeedge/viaduct/pkg/api"
+	"github.com/kubeedge/viaduct/pkg/comm"
 	"github.com/kubeedge/viaduct/pkg/conn"
 	"github.com/kubeedge/viaduct/pkg/lane"
 )
@@ -66,7 +67,7 @@ func (c *WSClient) Connect() (conn.Connection, error) {
 	// something wrong!!
 	var respMsg string
 	if resp != nil {
-		body, errRead := io.ReadAll(resp.Body)
+		body, errRead := io.ReadAll(io.LimitReader(resp.Body, comm.MaxReadLength))
 		if errRead == nil {
 			respMsg = fmt.Sprintf("response code: %d, response body: %s", resp.StatusCode, string(body))
 		} else {

neiba/kubeedge

Authored-by: vincentgoat <linguohui1@huawei.com>
(cherry picked from commit 7420c09bb311099f61d79a051ce5446976b1ea4e)

cloud/pkg/router/listener/http.go
@@ -2,6 +2,7 @@ package listener
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -153,7 +154,7 @@ func (rh *RestHandler) httpHandler(w http.ResponseWriter, r *http.Request) {
 			klog.Errorf("response convert error, msg id: %s, reason: %v", msgID, err)
 			return
 		}
-		body, err := ioutil.ReadAll(response.Body)
+		body, err := ioutil.ReadAll(io.LimitReader(response.Body, MaxMessageBytes))
 		if err != nil {
 			klog.Errorf("response body read error, msg id: %s, reason: %v", msgID, err)
 			return

edge/pkg/edgehub/certificate/certmanager.go
@@ -12,6 +12,7 @@ import (
 	"encoding/hex"
 	"encoding/pem"
 	"fmt"
+	"io"
 	"io/ioutil"
 	nethttp "net/http"
 	"strings"
@@ -253,8 +254,7 @@ func GetCACert(url string) ([]byte, error) {
 		return nil, err
 	}
 	defer res.Body.Close()
-
-	caCert, err := ioutil.ReadAll(res.Body)
+	caCert, err := ioutil.ReadAll(io.LimitReader(res.Body, constants.MaxRespBodyLength))
 	if err != nil {
 		return nil, err
 	}
@@ -285,7 +285,7 @@ func (cm *CertManager) GetEdgeCert(url string, capem []byte, cert tls.Certificat
 	}
 	defer res.Body.Close()
 
-	content, err := ioutil.ReadAll(res.Body)
+	content, err := ioutil.ReadAll(io.LimitReader(res.Body, constants.MaxRespBodyLength))
 	if err != nil {
 		return nil, nil, err
 	}

pkg/stream/message.go
@@ -23,6 +23,7 @@ import (
 	"io"
 	"io/ioutil"
 
+	"github.com/kubeedge/kubeedge/common/constants"
 	"k8s.io/klog/v2"
 )
 
@@ -89,7 +90,7 @@ func ReadMessageFromTunnel(r io.Reader) (*Message, error) {
 	if err != nil {
 		return nil, err
 	}
-	data, err := ioutil.ReadAll(buf)
+	data, err := ioutil.ReadAll(io.LimitReader(buf, constants.MaxRespBodyLength))
 	if err != nil {
 		return nil, err
 	}

staging/src/github.com/kubeedge/viaduct/pkg/client/ws.go
@@ -2,12 +2,14 @@ package client
 
 import (
 	"fmt"
+	"io"
 	"io/ioutil"
 
 	"k8s.io/klog/v2"
 
 	"github.com/gorilla/websocket"
 	"github.com/kubeedge/viaduct/pkg/api"
+	"github.com/kubeedge/viaduct/pkg/comm"
 	"github.com/kubeedge/viaduct/pkg/conn"
 	"github.com/kubeedge/viaduct/pkg/lane"
 	"github.com/kubeedge/viaduct/pkg/utils"
@@ -67,7 +69,7 @@ func (c *WSClient) Connect() (conn.Connection, error) {
 	// something wrong!!
 	var respMsg string
 	if resp != nil {
-		body, errRead := ioutil.ReadAll(resp.Body)
+		body, errRead := ioutil.ReadAll(io.LimitReader(resp.Body, comm.MaxReadLength))
 		if errRead == nil {
 			respMsg = fmt.Sprintf("response code: %d, response body: %s", resp.StatusCode, string(body))
 		} else {